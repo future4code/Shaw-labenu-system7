49d2b194fa23a4bd8479ff8584ab799e8a021717
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-06-22 18:47:36 -0300

    versao-base

---
+++ build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// dentro do index.ts, fazer importações e ativar o Express e Cors.
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+// a função express() inicia a aplicação web com express
+// os .use() ativam os módulos de Bodyparser e Cors
---
+++ src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// dentro do index.ts, fazer importações e ativar o Express e Cors.
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+// a função express() inicia a aplicação web com express
+// os .use() ativam os módulos de Bodyparser e Cors
+// esse código + essa importação para criar o servidor:
+// por performance, é bom o servidor ser o último trecho de código do documento
+const server = app.listen(3003, () => {
+    console.log(`Server is running in http://localhost:3003`);
+});
